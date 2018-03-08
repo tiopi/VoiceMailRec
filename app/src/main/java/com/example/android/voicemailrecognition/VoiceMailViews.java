834b672f37cf1d3663dde30c202f100f773eeaa9
Osmar Coronel <oac2@illinois.edu>
2018-03-08 17:33:07 -0600

    Made Android Project

---
+++ app/src/main/java/com/example/android/voicemailrecognition/VoiceMailViews.java
@@ -0,0 +1,13 @@
+package com.example.android.voicemailrecognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoiceMailViews extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_mail_views);
+    }
+}
